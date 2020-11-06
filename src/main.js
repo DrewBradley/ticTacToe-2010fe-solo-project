47cc93fcf522d6af12998f339582abf2a822c7e9
Drew Bradley <williamandrewb@gmail.com>
2020-11-05 18:50:01 -0700

    Declared all necessary query selectors

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// QUERY SELECTORS
+var announcement = document.querySelector('.announcement');
+var winCounter = document.querySelectorAll('.wins-counter');
+var gameGrid = document.querySelector('.game-grid');
+
