bb854fd0f25a2ccf3a1eaa61596de144ef70e477
Drew Bradley <williamandrewb@gmail.com>
2020-11-06 10:48:21 -0700

    Add Game constructor and win announcement function.

--- src/main.js
+++ src/main.js
@@ -1,67 +1,87 @@
 // QUERY SELECTORS
 var announcement = document.querySelector('.announcement');
 var winCounter = document.querySelectorAll('.wins-counter');
+var player1Box = document.querySelector('.player-section-one');
+var player2Box = document.querySelector('.player-section-two');
 var gameGrid = document.querySelector('.game-grid');
 var squares = document.querySelectorAll('.square');
 
 gameGrid.addEventListener('click', selectSquare);
 
-var player1arr = [];
-var player2arr = [];
-var gameArray = [];
+var game = new Game(Date.now())
 var player = 'player1';
 
-function showGame() {
-    showPlayerOne();
-    showPlayerTwo();
-
+function showGame(game) {
+    var p1Moves = game.player1.moves;
+    var p2Moves = game.player2.moves;
+    showPlayerOne(p1Moves);
+    showPlayerTwo(p2Moves);
+    game.findWin(p1Moves, p2Moves);
+    showWins(game);
+    if (announcement.innerText === '❌ WINS!' || announcement.innerText === '⭕️ WINS!'){
+        newGame(game.player1, game.player2);
+    }
 }
 
-function showPlayerOne(){
-    for(var i = 0; i < player1arr.length; i++){
+function newGame(p1, p2) {
+    game = new Game(Date.now(), p1, p2);
+}
+
+function showPlayerOne(p1Moves){
+    for(var i = 0; i < p1Moves.length; i++){
         squares.forEach(node => {
-            if (node.id === player1arr[i]){
+            if (node.id === p1Moves[i]){
                     node.innerText = '❌';
                 }
         })
     }
 }
 
-function showPlayerTwo(){
-    for(var i = 0; i < player2arr.length; i++){
+function showPlayerTwo(p2Moves){
+    for(var i = 0; i < p2Moves.length; i++){
         squares.forEach(node => {
-            if (node.id === player2arr[i]){
+            if (node.id === p2Moves[i]){
                     node.innerText = '⭕️';
                 }
         })
     }
 }
 
+function showWins(){
+    player1Box.children[1].innerText = `${game.player1.wins.length} WINS`;
+    player2Box.children[1].innerText = `${game.player2.wins.length} WINS`;
+}
+
 function selectSquare(event){
     var square = event.target;
     if (square.classList.contains('square')){
-        if (gameArray.includes(square.id)){
+        if (game.playedMoves.includes(square.id)){
             return;
         }
-        gameArray.push(square.id);
+        game.playedMoves.push(square.id);
         if (player === 'player1'){
-            player1arr.push(square.id);
+            game.player1.moves.push(square.id);
+
         }
         if (player === 'player2'){
-            player2arr.push(square.id);
+            game.player2.moves.push(square.id);
         }
     }
-    showGame();
+    showGame(game);
     switchPlayer();
 }
 
 function switchPlayer() {
     if (player === 'player1') {
         player = 'player2';
+        if (announcement.innerText !== '❌ WINS!')
+        announcement.innerText="⭕️'s TURN!"
         return;
     }
     if (player == 'player2') {
         player = 'player1';
+        if (announcement.innerText !== '⭕️ WINS!')
+        announcement.innerText="❌'s TURN!"
         return;
     }
 }
