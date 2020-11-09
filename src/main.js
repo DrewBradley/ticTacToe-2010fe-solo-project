204ad13fee19e66e11b7d2b857c0e623522e236f
Drew Bradley <williamandrewb@gmail.com>
2020-11-09 15:49:05 -0700

    Clean up WET CSS and JavaScript

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 // QUERY SELECTORS
 var announcement = document.querySelector('.announcement');
-var winCounter = document.querySelectorAll('.wins-counter');
 var player1Box = document.querySelector('.player1');
 var player2Box = document.querySelector('.player2');
 var gameGrid = document.querySelector('.game-grid');
@@ -35,57 +34,55 @@ function pageLoad() {
 }  
 
 function choosePlayers() {
-    game.player1.token = player1Token.value;
-    game.player2.token = player2Token.value;
-    showPlayers();
-    showGame(game);
-}
+    if (player1Token.value !== player2Token.value){
+        game.player1.token = player1Token.value;
+        game.player2.token = player2Token.value;
+        showPlayers();
+        showTurn();
+    }else{
+        alert("Players cannot be the same!")
+    };
+};
 
 function showPlayers(){
     startScreen.classList.add("hidden");
     gameGrid.classList.remove("hidden");
     player1Title.innerHTML = `<img src="assets/${game.player1.token}.png">`;
     player2Title.innerHTML = `<img src="assets/${game.player2.token}.png">`;
-}
+};
 
 function showGame(game) {
     showPlayed(game.currentPlayer);
     game.findWin(game.currentPlayer.moves);
-}
+};
 
 function selectSquare(event){
     var square = event.target;
     if (square.classList.contains("square")){
         if (game.playedMoves.includes(square.id)){
             return;
-        }
+        };
         game.playedMoves.push(square.id);
-        if (game.currentPlayer === game.player1){
-            game.player1.moves.push(square.id);
-
-        }
-        if (game.currentPlayer === game.player2){
-            game.player2.moves.push(square.id);
-        }
-    }
+        game.currentPlayer.moves.push(square.id);
+    };
     showGame(game);
     game.switchPlayer();
     showTurn();
-}
+};
 
 function showWin(token){
     announcement.innerText = `${token} WINS!`
     player1Box.children[1].innerText = `${game.player1.wins.length} WINS`;
     player2Box.children[1].innerText = `${game.player2.wins.length} WINS`;
-}
+};
 
 function declareDraw(){
     announcement.innerText = "DRAW!";
-}
+};
 
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-}
+};
 
 async function clearBoard(){
     disableSquares();
@@ -95,37 +92,37 @@ async function clearBoard(){
         node.innerText = '';
     })
     announcement.innerText = `${game.currentPlayer.token}'s TURN!`   
-}
+};
 
 function disableSquares(){
     squares.forEach(node => {
         node.disabled = true;
     })
-}
+};
 
 function enableSquares(){
     squares.forEach(node => {
         node.disabled = false;
     })
-}
+};
 
 function showPlayed(player){
     for(var i = 0; i < player.moves.length; i++){
         squares.forEach(node => {
             if (node.id === player.moves[i]){
-                    node.innerHTML = `<img src="assets/${player.token}.png">`;
-                }
+                node.innerHTML = `<img src="assets/${player.token}.png">`;
+            };
         })
-    }
-}
+    };
+};
 
 function showTurn() {
     if (announcement.innerText.includes("WINS!")){
         return 
-    }
+    };
     if (announcement.innerText.includes("DRAW!")){
         return
-    }
+    };
     announcement.innerText=`${game.currentPlayer.token}'s TURN!`
         return;
-}
+};
